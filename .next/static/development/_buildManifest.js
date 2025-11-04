5986e372aef75cd92480c884e429378eb10cd37f
MeKeshavPandey <148474224+MeKeshavPandey@users.noreply.github.com>
2025-11-04 23:42:31 +0530

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/showschools":["static\u002Fchunks\u002Fpages\u002Fshowschools.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fshowschools"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
